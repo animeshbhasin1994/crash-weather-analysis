25b6a552130edfcc20f8c3400b450f924fb0cbff
shivamojha2 <84263986+shivamojha2@users.noreply.github.com>
2021-11-29 03:33:23 -0500

    Updated functions

--- get_weather.py
+++ get_weather.py
@@ -1,6 +1,6 @@
 """
 Author : Shivam Ojha
-Version : 1.1
+Version : 2
 Version Date : 29th Nov 2022
 Description : This script is to scrape the weather data from
 https://www.wunderground.com/history/daily/us/ny/new-york-city/KJFK/date/2021-11-18
@@ -14,6 +14,7 @@ from sqlalchemy import create_engine
 from bs4 import BeautifulSoup
 from selenium import webdriver
 import time
+import logging
 
 from selenium.webdriver.firefox.options import Options
 
@@ -32,60 +33,61 @@ def download_file(link):
     return soup
 
 def scrape_file(soup):
+    try:
+        dateString_list = []
+        temperature_list = []
+        dewPoint_list = []
+        humidity_list = []
+        windcardinal_list = []
+        windSpeed_list = []
+        windGust_list = []
+        pressure_list = []
+        precipRate_list = []
+        condition_list = []
 
-    dateString_list = []
-    temperature_list = []
-    dewPoint_list = []
-    humidity_list = []
-    windcardinal_list = []
-    windSpeed_list = []
-    windGust_list = []
-    pressure_list = []
-    precipRate_list = []
-    condition_list = []
+        for row in soup.find_all("td", {'class':"mat-cell cdk-cell cdk-column-dateString mat-column-dateString ng-star-inserted"}):
+            dateString_list.append(row.text.split()[0])
 
-    for row in soup.find_all("td", {'class':"mat-cell cdk-cell cdk-column-dateString mat-column-dateString ng-star-inserted"}):
-        dateString_list.append(row.text.split()[0])
+        for row in soup.find_all("td", {'class':"mat-cell cdk-cell cdk-column-temperature mat-column-temperature ng-star-inserted"}):
+            temperature_list.append(row.text.split()[0])
 
-    for row in soup.find_all("td", {'class':"mat-cell cdk-cell cdk-column-temperature mat-column-temperature ng-star-inserted"}):
-        temperature_list.append(row.text.split()[0])
+        for row in soup.find_all("td", {'class':"mat-cell cdk-cell cdk-column-dewPoint mat-column-dewPoint ng-star-inserted"}):
+            dewPoint_list.append(row.text.split()[0])
 
-    for row in soup.find_all("td", {'class':"mat-cell cdk-cell cdk-column-dewPoint mat-column-dewPoint ng-star-inserted"}):
-        dewPoint_list.append(row.text.split()[0])
+        for row in soup.find_all("td", {
+            'class': "mat-cell cdk-cell cdk-column-humidity mat-column-humidity ng-star-inserted"}):
+            humidity_list.append(row.text.split()[0])
 
-    for row in soup.find_all("td", {
-        'class': "mat-cell cdk-cell cdk-column-humidity mat-column-humidity ng-star-inserted"}):
-        humidity_list.append(row.text.split()[0])
+        for row in soup.find_all("td", {
+            'class': "mat-cell cdk-cell cdk-column-windcardinal mat-column-windcardinal ng-star-inserted"}):
+            windcardinal_list.append(row.text.split()[0])
 
-    for row in soup.find_all("td", {
-        'class': "mat-cell cdk-cell cdk-column-windcardinal mat-column-windcardinal ng-star-inserted"}):
-        windcardinal_list.append(row.text.split()[0])
+        for row in soup.find_all("td",
+                                {'class': "mat-cell cdk-cell cdk-column-windSpeed mat-column-windSpeed ng-star-inserted"}):
+            windSpeed_list.append(row.text.split()[0])
+        for row in soup.find_all("td", {
+            "mat-cell cdk-cell cdk-column-windGust mat-column-windGust ng-star-inserted"}):
+            windGust_list.append(row.text.split()[0])
 
-    for row in soup.find_all("td",
-                             {'class': "mat-cell cdk-cell cdk-column-windSpeed mat-column-windSpeed ng-star-inserted"}):
-        windSpeed_list.append(row.text.split()[0])
-    for row in soup.find_all("td", {
-        "mat-cell cdk-cell cdk-column-windGust mat-column-windGust ng-star-inserted"}):
-        windGust_list.append(row.text.split()[0])
+        for row in soup.find_all("td", {
+            'class': "mat-cell cdk-cell cdk-column-pressure mat-column-pressure ng-star-inserted"}):
+            pressure_list.append(row.text.split()[0])
 
-    for row in soup.find_all("td", {
-        'class': "mat-cell cdk-cell cdk-column-pressure mat-column-pressure ng-star-inserted"}):
-        pressure_list.append(row.text.split()[0])
+        for row in soup.find_all("td",
+                                {'class':  "mat-cell cdk-cell cdk-column-precipRate mat-column-precipRate ng-star-inserted"}):
+            precipRate_list.append(row.text.split()[0])
 
-    for row in soup.find_all("td",
-                             {'class':  "mat-cell cdk-cell cdk-column-precipRate mat-column-precipRate ng-star-inserted"}):
-        precipRate_list.append(row.text.split()[0])
+        for row in soup.find_all("td",
+                                {'class': "mat-cell cdk-cell cdk-column-condition mat-column-condition ng-star-inserted"}):
+            condition_list.append(row.text.split()[0])
 
-    for row in soup.find_all("td",
-                             {'class': "mat-cell cdk-cell cdk-column-condition mat-column-condition ng-star-inserted"}):
-        condition_list.append(row.text.split()[0])
-
-    d = {'Hour': dateString_list, 'Temp':temperature_list, 'Dew' : dewPoint_list, 'humidity' :humidity_list,
-         'Wind Cardinal' :windcardinal_list, 'Wind Speed' : windSpeed_list, 'Wind Gust': windGust_list,
-         'Pressure List': pressure_list, 'Precip Rate': precipRate_list, 'Condition':condition_list}
-    df = pd.DataFrame(d)
-
-    return df
+        d = {'Hour': dateString_list, 'Temp':temperature_list, 'Dew' : dewPoint_list, 'humidity' :humidity_list,
+            'Wind Cardinal' :windcardinal_list, 'Wind Speed' : windSpeed_list, 'Wind Gust': windGust_list,
+            'Pressure List': pressure_list, 'Precip Rate': precipRate_list, 'Condition':condition_list}
+        df = pd.DataFrame(d)
+        return df
+    except IndexError:
+        return None
 
 
 def generate_links(yesterday_date):
@@ -114,19 +116,19 @@ def generate_links(yesterday_date):
     return links
 
 
-def generate_incremental_links(date_obj):
-    link_format = "https://www.wunderground.com/history/daily/us/ny/new-york-city/{}/date/{}}"
+def generate_incremental_links(given_date):
+    link_format = "https://www.wunderground.com/history/daily/us/ny/new-york-city/{}/date/{}"
 
     today = date.today()
     today_str = today.strftime("%Y-%m-%d")
     today_obj = datetime.datetime.strptime(today_str, "%Y-%m-%d")
 
     links = []
-    date_str = datetime.datetime.strptime(date_obj, "%Y-%m-%d")
-    links.append(link_format.format('KJFK', date_str))
+    links.append(link_format.format('KJFK', given_date))
+    date_obj = datetime.datetime.strptime(given_date, "%Y-%m-%d")
     while date_obj < today_obj:
         date_obj = date_obj + datetime.timedelta(days=1)
-        date_str = datetime.datetime.strptime(date_obj, "%Y-%m-%d")
+        date_str = date_obj.strftime("%Y-%m-%d")
         links.append(link_format.format('KJFK', date_str))
 
     return links
@@ -135,7 +137,7 @@ def generate_incremental_links(date_obj):
 
 def get_date_max_date_in_db(engine, schema_name, table_name):
     #sql_statement = '''select max(Date + interval '1' day) from {}.{};'''.format(schema_name, table_name)
-    sql_statement = '''select max(Date) from {}.{};'''.format(schema_name, table_name)
+    sql_statement = '''select max({}."Date") from {}.{};'''.format(table_name, schema_name, table_name)
 
     with engine.connect() as connection:
         result = connection.execute(sql_statement)
@@ -163,22 +165,25 @@ def main():
     else:
         #load incremental data, compared to last date in db
         max_date = get_date_max_date_in_db(engine, schema_name, weather_table_name)
-        links = generate_incremental_links(max_date)
+        links = generate_incremental_links(str(max_date))
 
     for i, link in enumerate(links):
         soup = download_file(link)
         df = scrape_file(soup)
-        df['Date'] = link.split('/')[-1]
-        df['station'] = 'KJFK'
+        if isinstance(df, pd.core.frame.DataFrame):
+            df['Date'] = link.split('/')[-1]
+            df['station'] = 'KJFK'
+            print("Storing values for date: %s", link.split('/')[-1])
 
-        write_to_db(df, table_name= weather_table_name,schema_name = schema_name,engine= engine,date_list_str= yes_date_str,
-                    station = 'KJFK')
+            write_to_db(df, table_name= weather_table_name,schema_name = schema_name,engine= engine,date_list_str= link.split('/')[-1],
+                        station = 'KJFK')
 
 
 def write_to_db(df, table_name, schema_name, engine, date_list_str, station):
-    #sql_statement = '''delete from {}.{} where "Date" in ('{}') and station = '{}';'''.format(schema_name, table_name,
-    #                                                                      date_list_str, station)
-    #engine.execute(sql_statement)
+    # Remove duplicates
+    sql_statement = '''delete from {}.{} where {}."Date" in ('{}')'''.format(schema_name, table_name, table_name,
+                                                                          date_list_str)
+    engine.execute(sql_statement)
     df.to_sql(name=table_name, schema=schema_name, con=engine, if_exists='append', index=False)
 
 
